8fb4b70403e1fc72bd81d8c247ebd0819d1f5446
HillaShx <hilla.sh@gmail.com>
2022-10-26 20:58:10 +0300

    add files

---
+++ frontend/src/tests/ci.test.js
@@ -0,0 +1,14 @@
+describe("CI tests", () => {
+    it("test calc 1", () => {
+      expect(1 + 1).toEqual(2);
+    });
+  
+    it("test calc 2", () => {
+      expect(3 * 2).toEqual(7);
+    });
+  
+    it("test calc 3", () => {
+      expect(10 / 5).toEqual(2);
+    });
+  });
+  
